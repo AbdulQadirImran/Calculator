aef6fe9da00ac071d899e6aaeae264bb373537ff
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-12 21:57:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const display = document.querySelector(".display");
-
 const buttons = document.querySelectorAll("button");
-
 const specialChars = ["%", "*", "/", "-", "+", "="];
-
 let output = "";
 
 //Define function to calculate based on button clicked.
